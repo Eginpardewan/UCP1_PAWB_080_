5026bc5e4a0468572ab06658cf72d94b7d2dc835
Egin Pardewan <eginpardewan@gmail.com>
2024-11-23 16:38:19 +0700

    index.js

--- index.js
+++ index.js
@@ -6,15 +6,3 @@ import pupukRoute from "./routes/pupuk.js";
 const app = express();
 const PORT = 8000;
 
-app.use(bodyParser.json());
-
-
-app.use("/Bibit", bibitRoute);
-app.use("/Pupuk", pupukRoute);
-app.get("/", (req, res) =>{
-    console.log(['GET ROUTE']);
-    res.send("Selamat Datang Di Website Petani Serderhana");
-});
-
-app.use(bodyParser.json());
-app.listen(PORT, () => console.log(`server berjalan di port : http://localhost:${PORT}`));
