004949d44c01b1583ce62e43036fb1ab1fc5fe39
Egin Pardewan <eginpardewan@gmail.com>
2024-11-23 16:42:16 +0700

    Route default untuk root ("/")

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ app.use(bodyParser.json());
 
 app.use("/Bibit", bibitRoute);
 app.use("/Pupuk", pupukRoute);
+app.get("/", (req, res) =>{
+    console.log(['GET ROUTE']);
+    res.send("Selamat Datang Di Website Petani Serderhana");
+});
+
