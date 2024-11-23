8cb55fa2f17b6515f8bed586e84d8c50650b2b1f
Egin Pardewan <eginpardewan@gmail.com>
2024-11-23 17:13:28 +0700

    mengubah post bibit tanaman

--- routes/bibit-tanaman.js
+++ routes/bibit-tanaman.js
@@ -39,9 +39,13 @@ router.get("/", (req, res) => {
 // POST Method untuk menambah data bibit baru
 router.post("/", (req, res) => {
     const { name, type, category } = req.body;
+    if (!name || !type || !category) {
+        return res.status(400).json({ message: "Semua field wajib diisi." });
+    }
     const id = bibit.length ? bibit[bibit.length - 1].id + 1 : 1;
     const newBibit = { id, name, type, category };
     bibit.push(newBibit);
+
     res.status(201).json(newBibit);
 });
 
